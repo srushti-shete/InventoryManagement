fe393d016e8c45b12618265a3e51eba4c3a626ea
Srushti Shete <shetesrushti11@gmail.com>
2023-08-29 00:46:31 +0530

    Created a user module

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Home page");
+});
+
+// Connect to DB and start server
+const PORT = process.env.PORT || 5000;
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server Running on port ${PORT}`)
+        })
+    })
+    .catch((err) => console.log(err))
